77824a79d141c18cb56af29f2fc11f25f538f9ec
Divya <ramdivyamadhu@gmail.com>
2021-09-17 12:50:39 +0530

    Initial Commit

---
+++ CalculateInterest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLibrary
+{
+    public class CalculateInterest : ICalculateInterest
+    {
+        public int Calculate(int PrincipleAmount, int RateOfInterest, int NOOfYears)
+        {
+            int SI = (PrincipleAmount * RateOfInterest * NOOfYears) / 100;
+            return SI;
+        }
+    }
+}
---
+++ ICalculateInterest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingLibrary
+{
+    public interface ICalculateInterest
+    {
+        
+            int Calculate(int PrincipleAmount, int RateOfInterest, int NOOfYears);
+        
+    }
+}
